abc84b0600291ed7720003750c208c308a2f1280
Kazumen <yura07042004@gmail.com>
2023-04-19 00:40:10 +0300

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import ProductList from './components/ProductList/ProductList';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import EditProduct from './components/EditProduct/EditProduct';
+import AddProduct from './components/AddProduct/AddProduct';
 
 function App() {
 
@@ -28,7 +33,27 @@ function App() {
   return (
     <div>
       <Header/>
-      <ProductList products={ products} deleteProduct={ deleteProduct } />
+      <BrowserRouter>
+         <Routes>
+          <Route path='/' element ={<ProductList products={ products} deleteProduct={ deleteProduct } />}> 
+          </Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/add' element={<AddProduct/>}></Route>
+          <Route path='/edit' element={<EditProduct/>}></Route>
+
+         </Routes>
+      </BrowserRouter>
+
+
+
+
+
+
+
+
+
+      
       
       
     </div>
